9961c4b2dc1833d6c2a1b3dbe15a7c2f718aaf89
Ramanakishore-Peroji <perojiramanakishore@gmail.com>
2021-06-04 09:41:03 +0530

    test api

--- src/Components/Public/Home.js
+++ src/Components/Public/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
-function Home() {
+function Home(props) {
+
+    const message = () => {
+        axios.post('https://department-library.herokuapp.com/admin/test').then(response => {
+            alert(response.data.test);
+            props.history.push('/about');
+        }).catch(error => {
+            if (error) alert("Something went wrong. Please try again later.");
+        });
+    }
+
     return (
-        <div>
+        <div onLoad={message}>
             <h1 className='info'>An interface for RGUKT Basar library.</h1>
             <h2 className='info'>Students: Search, Favorite and Order Books at your fingertips. No more hassle.</h2>
             <h2 className='info'>Faculty: Search, Tag and Rate Books to help students.</h2>
