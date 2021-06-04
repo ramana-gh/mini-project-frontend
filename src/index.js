5feefe7dcde462ee4c20f883866798ad2fc70ab2
Ramanakishore-Peroji <perojiramanakishore@gmail.com>
2021-06-04 18:17:09 +0530

    commit on Fri 4 Jun 2021 05:13 PM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Admin from './Dashboards/Admin';
+import Faculty from './Dashboards/Faculty';
+import Student from './Dashboards/Student';
 import Public from './Dashboards/Public';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Router>
         <div>
             <Switch>
+                <Route path='/admin' component={Admin} />
+                <Route path='/faculty' component={Faculty} />
+                <Route path='/student' component={Student} />
                 <Route component={Public} />
             </Switch>
             <div className='footer'>
